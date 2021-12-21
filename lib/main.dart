52a71afcf3a5769c6cb6db45a9de4f6d6725d709
eunki7 <outsider7224@hanmail.net>
2021-12-21 16:29:57 +0900

    flutter widget life cycle part commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:ex1/rabbit.dart';
 import 'package:ex1/stateful_sample_widget.dart';
 import 'package:ex1/stateless_sample_widget.dart';
@@ -7,8 +9,26 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int value = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        value++;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +41,11 @@ class MyApp extends StatelessWidget {
       //   title : "구멍이 없는 박스로 실험하는자",
       //   rabbit : Rabbit(name : "개남토끼1", state : RabbitState.SLEEP)
       // ),
-      home: StatefulSampleWidget(
+      home: value > 10 ?
+      Container() :
+      StatefulSampleWidget(
           title : "구멍이 있는 박스로 실험하는자",
+          value: value,
           rabbit : Rabbit(name : "개남토끼2", state : RabbitState.SLEEP)
       ),
     );
--- lib/stateful_sample_widget.dart
+++ lib/stateful_sample_widget.dart
@@ -1,15 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-
 import 'rabbit.dart';
 
 class StatefulSampleWidget extends StatefulWidget {
 
   String title;
   Rabbit rabbit;
+  int value;
 
-  StatefulSampleWidget({Key? key, required this.title, required this.rabbit}) : super(key: key);
+  StatefulSampleWidget({Key? key, required this.title, required this.rabbit,  required this.value}) : super(key: key);
 
   @override
   _StatefulSampleWidgetState createState() => _StatefulSampleWidgetState();
@@ -17,37 +17,51 @@ class StatefulSampleWidget extends StatefulWidget {
 
 class _StatefulSampleWidgetState extends State<StatefulSampleWidget> {
 
+  bool stateBool = false;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      int index = timer.tick % 4;
-      print(index);
-
-      setState(() {
-        switch (index) {
-          case 0:
-            widget.rabbit.updateState(RabbitState.SLEEP);
-            break;
-          case 1:
-            widget.rabbit.updateState(RabbitState.WALK);
-            break;
-          case 2:
-            widget.rabbit.updateState(RabbitState.RUN);
-            break;
-          case 3:
-            widget.rabbit.updateState(RabbitState.EAT);
-            break;
-        }
-        print(widget.rabbit.state);
-      });
-
-    });
+    print("_StatefulSampleWidgetState init state!!");
   }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print('dispose!!');
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies(){
+    super.didChangeDependencies();
+    print("didchange dependencies!!");
+    print(MediaQuery.of(context).size);
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    // TODO: implement setState
+    super.setState(fn);
+    if(mounted) {
+      print("Setstate!!");
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant StatefulSampleWidget oldWidget) {
+    // TODO: implement didUpdateWidget
+    print("old : ${oldWidget.value} <> cur : ${widget.value}");
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.value != widget.value) {
+      print('didUpdateWidget!!');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("build!!");
     return Scaffold(
       appBar: AppBar(title : Text(widget.title)),
       body: Container(
@@ -58,6 +72,14 @@ class _StatefulSampleWidgetState extends State<StatefulSampleWidget> {
                     fontSize: 20
                 )),
           )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            stateBool = !stateBool;
+          });
+        },
+        child: Text("Button"),
       ),
     );
   }
